e100a5cc705854a07389d573bfc5837d2fb9facc
qasim <qasim.shafique875@gmail.com>
2023-02-21 14:26:11 +0500

    first commit

--- Admin_panel/src/Pages/Student/UpdateStudent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function UpdateBook() {
-    return <div>UpdateBook</div>;
-}
-
-export default UpdateBook;
--- Admin_panel/src/store/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { createStore } from 'redux';
-import { store } from './reducer';
-// ==============================|| REDUX - MAIN STORE ||============================== //
-
-// const stores = createStore(store);
-const persister = 'Free';
-
-export { store, persister };
